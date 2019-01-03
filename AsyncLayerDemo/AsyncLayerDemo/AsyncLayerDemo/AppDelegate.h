7b566b639a3c9ff1f81d4c79ffeacae64f915c93
Daniel <yebin@gtjas.com>
2019-01-03 23:30:09 +0800

    异步渲染demo提交

---
+++ AsyncLayerDemo/AsyncLayerDemo/AsyncLayerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AsyncLayerDemo
+//
+//  Created by keke on 2019/1/3.
+//  Copyright © 2019 kekeyezi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AsyncLayerDemo/AsyncLayerDemo/AsyncLayerDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  AsyncLayerDemo
+//
+//  Created by keke on 2019/1/3.
+//  Copyright © 2019 kekeyezi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
