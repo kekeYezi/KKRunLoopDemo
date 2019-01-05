72a69e8953f3f3af7aa26afd4de9af0c6aa6bf14
Daniel <yebin@gtjas.com>
2019-01-05 17:12:12 +0800

    提交重启RunLoop Demo

--- KKRunLoopDemo/KKRunLoopDemo/ViewControllers/RestartViewController.h
+++ KKRunLoopDemo/KKRunLoopDemo/ViewControllers/RestartViewController.h
@@ -6,6 +6,11 @@
 //  Copyright © 2019 kekeyezi. All rights reserved.
 //
 
+//  记录2019-1-5
+//  问题1:为啥都用的同一个方法 重启 RunLoop ，这个思想是怎么来了？ Apple介绍么
+//  问题2:即使重启RunLoop不崩溃了，后续是对App更好还是会代码更加不稳定的代价？
+//  问题3:为什么只生效一次 第二次直接 拦截不住 直接崩溃？
+
 #import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
--- KKRunLoopDemo/KKRunLoopDemo/ViewControllers/RestartViewController.m
+++ KKRunLoopDemo/KKRunLoopDemo/ViewControllers/RestartViewController.m
@@ -10,52 +10,21 @@
 #include <libkern/OSAtomic.h>
 #include <execinfo.h>
 
-NSString * const kSignalExceptionName = @"kSignalExceptionName";
-NSString * const kSignalKey = @"kSignalKey";
-NSString * const kCaughtExceptionStackInfoKey = @"kCaughtExceptionStackInfoKey";
-
 @interface RestartViewController ()
 
 @end
 
 @implementation RestartViewController
 
-void eHandler(NSException *exception){
-    NSString *message = [NSString stringWithFormat:@"崩溃原因如下:\n%@\n%@",
-                         [exception reason],
-                         [[exception userInfo] objectForKey:kCaughtExceptionStackInfoKey]];
-    NSLog(@"%@",message);
-    
-    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"程序崩溃了"
-                                                    message:@"如果你能让程序起死回生，那你的决定是？"
-                                                   delegate:nil
-                                          cancelButtonTitle:@"崩就蹦吧"
-                                          otherButtonTitles:@"起死回生", nil];
-    [alert show];
-    
+void eHandler(NSException *exception) {
+
     CFRunLoopRef runLoop = CFRunLoopGetCurrent();
     CFArrayRef allModes = CFRunLoopCopyAllModes(runLoop);
     
-    while (1) {
+    while (YES) {
         for (NSString *mode in (__bridge NSArray *)allModes) {
             CFRunLoopRunInMode((CFStringRef)mode, 0.001, false);
         }
-    }
-    
-    CFRelease(allModes);
-    
-    NSSetUncaughtExceptionHandler(NULL);
-    signal(SIGABRT, SIG_DFL);
-    signal(SIGILL, SIG_DFL);
-    signal(SIGSEGV, SIG_DFL);
-    signal(SIGFPE, SIG_DFL);
-    signal(SIGBUS, SIG_DFL);
-    signal(SIGPIPE, SIG_DFL);
-    
-    if ([[exception name] isEqual:kSignalExceptionName]) {
-        kill(getpid(), [[[exception userInfo] objectForKey:kSignalKey] intValue]);
-    } else {
-        [exception raise];
     }
 
 }
@@ -65,7 +34,7 @@ void eHandler(NSException *exception){
     NSSetUncaughtExceptionHandler(&eHandler);
     
     self.view.backgroundColor = [UIColor whiteColor];
-    self.title = @"复活";
+    self.title = @"重启RunLoop";
     // Do any additional setup after loading the view.
 }
 
